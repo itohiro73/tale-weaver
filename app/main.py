1eb5c5bf6e5add19d0cbf993847915777b8d4799
Hiroshi Ito <itohiro73@gmail.com>
2023-03-19 20:17:13 +0900

    Fix CORS issue.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .mock_data import mock_story_parts
 from .routes import router as story_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(story_router)
 
