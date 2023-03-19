a3733062358d3b4cd8e9d510c6f4cebf2b428df0
Hiroshi Ito <itohiro73@gmail.com>
2023-03-19 16:55:00 +0900

    Add RESTful API routing.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
+from .routes import router as story_router
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(story_router)
 
