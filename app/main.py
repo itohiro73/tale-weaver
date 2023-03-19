cfdf39d67bb9cd8376cb7532107fb829189f61ff
Hiroshi Ito <itohiro73@gmail.com>
2023-03-19 20:09:25 +0900

    Add mock stories and api to return the first part.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from .mock_data import mock_story_parts
 from .routes import router as story_router
 
 app = FastAPI()
 
 app.include_router(story_router)
 
+@app.get("/api/story/first-part")
+async def get_first_part():
+    return mock_story_parts[0]
---
+++ app/mock_data.py
@@ -0,0 +1,17 @@
+mock_story_parts = [
+    {
+        "id": 1,
+        "title": "The Beginning",
+        "content": "Once upon a time, in a small village, there lived a young hero...",
+    },
+    {
+        "id": 2,
+        "title": "The Quest",
+        "content": "One day, the hero received a letter from the king, asking for help...",
+    },
+    {
+        "id": 3,
+        "title": "The Climax",
+        "content": "After a long and perilous journey, the hero finally faced the dragon...",
+    },
+]
